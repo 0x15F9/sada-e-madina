66ed110e18e66b8df9fad9c636ee47a280118014
0x15F9 <isfaaqg@gmail.com>
2019-07-26 17:37:40 +0400

    add some pages

---
+++ pages/Dial/Dial.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import styles from "./_style";
+
+const Dial = () => (
+  <View style={styles.container}>
+    <Text>Call</Text>
+  </View>
+);
+
+export default Dial;
---
+++ pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import styles from "./_style";
+
+const Home = () => (
+  <View style={styles.container}>
+    <Text>Sada-e-Madina</Text>
+    <Button title="Call" />
+  </View>
+);
+
+export default Home;
