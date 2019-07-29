0f42014a0723450cbbfffed293ea5228c9895970
0x15F9 <isfaaqg@gmail.com>
2019-07-29 22:51:40 +0400

    layout call screen

--- screens/CallScreen/index.js
+++ screens/CallScreen/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ProgressViewIOS } from "react-native";
 class CallScreen extends Component {
+  state = { progress: 0 };
+  componentWillMount() {
+    this.setState({
+      callList: this.props.navigation.getParam("callList", [])
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      numList: this.state.callList.length,
+      numCalled: 0,
+      numResponded: 0,
+      numUnresponsive: 0
+    });
+  }
+
   render() {
+    const { numList, numCalled, numResponded, numUnresponsive } = this.state;
     return (
       <View>
-        <Text>Call Screen</Text>
+        <Text>Number of People in List: {numList}</Text>
+        <Text>Number of People Called: {numCalled}</Text>
+        <Text>
+          Number of People who responed (accepted or rejected): {numResponded}
+        </Text>
+        <Text>Number of People who did not respond: {numUnresponsive}</Text>
+        <ProgressViewIOS
+          progress={this.state.progress}
+          progressViewStyle="bar"
+        />
       </View>
     );
   }
