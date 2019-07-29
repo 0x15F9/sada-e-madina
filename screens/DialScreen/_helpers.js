d653c3e3ff37d856edf994517f750735ec5680f0
0x15F9 <isfaaqg@gmail.com>
2019-07-29 18:57:07 +0400

    complete select contacts

--- screens/DialScreen/_helpers.js
+++ screens/DialScreen/_helpers.js
@@ -1,12 +1,20 @@
 import * as Contacts from "expo-contacts";
 
 export const getContacts = () => {
-  Contacts.getContactsAsync({
-    fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers]
-  })
-    .then(contacts => [{ name: "Sabina G", phoneNumber: "1245783" }])
-    .catch(err => {
-      return "Error";
-      console.log(err);
+  try {
+    // FIXME:add await
+    const contacts = Contacts.getContactsAsync({
+      fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers]
     });
+    return contacts.data
+      .filter(contact => contact.phoneNumbers !== undefined) // Get contacts which have phone numbers
+      .map(({ id, name, phoneNumbers }) => ({
+        id,
+        name,
+        phoneNumber: phoneNumbers[0].number
+      }));
+  } catch (error) {
+    return "Error";
+    console.log(error);
+  }
 };
--- screens/DialScreen/index.js
+++ screens/DialScreen/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import Contacts from "../../components/Contacts";
 import * as _Contacts from "expo-contacts";
 import styles from "./_style";
 import { getContacts } from "./_helpers";
+import { ListItem, CheckBox } from "react-native-elements";
 
 class DialScreen extends React.Component {
-  state = {};
+  state = { contacts: [] };
+
+  onItemSelected = id => {
+    this.setState({
+      contacts: this.state.contacts.map(contact =>
+        contact.id === id
+          ? { ...contact, selected: !contact.selected }
+          : contact
+      )
+    });
+  };
+
+  onDialPressed = () => {
+    console.log(this.state.contacts.filter(contact => contact.selected));
+  };
 
   componentDidMount() {
+    // TODO: Move to helpers
     _Contacts
       .getContactsAsync({
         fields: [_Contacts.Fields.Name, _Contacts.Fields.PhoneNumbers]
@@ -19,7 +34,8 @@ class DialScreen extends React.Component {
           .map(({ id, name, phoneNumbers }) => ({
             id,
             name,
-            phoneNumber: phoneNumbers[0].number
+            phoneNumber: phoneNumbers[0].number,
+            selected: false
           }));
         this.setState({ contacts: c });
       })
@@ -33,19 +49,26 @@ class DialScreen extends React.Component {
     return (
       <View style={styles.container}>
         {this.state.contacts ? (
-          this.state.contacts.map(({ id, name, phoneNumber }) => (
-            <Contacts key={id} name={name} phoneNumber={phoneNumber} />
+          this.state.contacts.map(({ id, name, phoneNumber, selected }) => (
+            <ListItem
+              key={id}
+              title={name}
+              subtitle={phoneNumber}
+              rightElement={
+                <CheckBox
+                  checked={selected}
+                  onPress={() => this.onItemSelected(id)}
+                />
+              }
+              bottomDivider={true}
+              onPress={() => this.onItemSelected(id)}
+            />
           ))
         ) : (
           <Text>Loading...</Text>
         )}
 
-        <Button
-          title="Dial"
-          onPress={() => {
-            console.log(this.state);
-          }}
-        />
+        <Button title="Dial" onPress={this.onDialPressed} />
       </View>
     );
   }
